851934b5d2f2282b78c6fd2b09925475ec16da81
Clare Buckley <clarebuckley1@hotmail.com>
2020-04-11 19:38:47 +0100

    use react instead, get basic response from api

--- back-end/index.js
+++
@@ -1,7 +0,0 @@
-const http = require("http");
-
-http.createServer((request, response) => {
-    response.writeHead(200, { "Content-Type": "text/plain" });
-    response.write("Listening on 8080");
-    response.end();
-}).listen(8080);
